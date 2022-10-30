54ddc664838e07527696be9d1e05ddb7c4f0e0d5
barserkaua <barserkaua@gmail.com>
2022-10-30 21:21:25 +0200

    fix: :bug: fixed test conditions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+
 import { store } from "../app/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
